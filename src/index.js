37c9ddcd38d745151345d98a54fab775ae4c3b6c
BIa <ana.ferreira@luizalabs.com>
2023-09-22 11:07:27 -0300

    codigo

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//first archive to be executed
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+//the first page that will be executed when the page is loaded
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
