35b16013576d2a8730139e6396bdb1794a607bd1
bfontes <fontesfbs@gmail.com>
2023-10-09 23:09:54 -0300

    add some code

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setShowForm(false);
+  };
+
+  const showFormHandler = () => {
+    setShowForm(true);
+  };
+
+  const cancelHandler = () => {
+    setShowForm(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!showForm && (
+        <button onClick={showFormHandler} type="submit">
+          Add New Expense
+        </button>
+      )}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelHandler}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
